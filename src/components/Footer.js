584cd9bc360a208f8131608c6ee4e5da5410c498
raj_khan <meherullah97@gmail.com>
2020-06-03 14:21:09 +0600

    Project images showed through Unsplash API

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,10 +12,10 @@ class Footer extends Component {
                                 <h3>+8801915151681</h3>
                                 <div className="footer-menu">
                                     <ul>
-                                        <li><a href="">Facebook</a></li>
-                                        <li><a href="">Twitter</a></li>
-                                        <li><a href="">Linkedin</a></li>
-                                        <li><a href="">Youtube</a></li>
+                                        <li><a href="/">Facebook</a></li>
+                                        <li><a href="/">Twitter</a></li>
+                                        <li><a href="/">Linkedin</a></li>
+                                        <li><a href="/">Youtube</a></li>
                                     </ul>
                                 </div>
                                 <div className="copyright-text">Lorem ipsum dolor sit.</div>
--- src/components/LatestPhotos.js
+++ src/components/LatestPhotos.js
@@ -1,19 +1,73 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
 
 class LatestPhotos extends Component {
-    render() {
-        return (
-            <div className="col-lg-3">
-                <div className="single-photo-item">
-                    <a href="d-block">
-                        <img src="http://placehold.it/600x350" alt=""/>
-                        <h5>Project Name</h5>
-                        <p className="cat-name">Category Name</p>
-                    </a>
 
-                </div>
-            </div>
+    state = {
+        photos: [],
+        page: 1,
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('https://api.unsplash.com/photos/?client_id=wu7CUeow5HCvgKDOIZ3801ePPSO4A_UwuyBYyycDQaA&per_page=16&page='+this.state.page).then(
+            res => this.setState({
+                photos: res.data,
+                loading: false
+            })
         );
+        window.scrollTo(0, 0)
+    }
+
+    loadNextPage = (e) => {
+        this.setState({page: this.state.page + 1});
+
+        axios.get('https://api.unsplash.com/photos/?client_id=wu7CUeow5HCvgKDOIZ3801ePPSO4A_UwuyBYyycDQaA&per_page=16&page='+this.state.page).then(
+            res => this.setState({
+                photos: res.data,
+                loading: false
+            })
+        )
+    }
+
+    render() {
+
+        if(this.state.loading === true)
+        {
+            return (
+                <div className="text-center text-danger">Photos are Loading</div>
+            )
+        }
+
+        return (
+            <React.Fragment>
+                {
+                    this.state.photos.map((photo) => (
+                        <div key={photo.id} className="col-lg-3">
+                            <div className="single-photo-item">
+                                <a className="d-block" href="/">
+                                    <div className="photo-wrapper">
+                                        <img src={photo.urls.small} alt={photo.description}/>
+                                    </div>
+
+                                    <h5>{photo.description}</h5>
+                                    <p className="cat-name">by - {photo.user.first_name} {photo.user.last_name}</p>
+                                </a>
+
+                            </div>
+                        </div>
+                    ))
+                }
+
+
+                <div className="col-lg-12">
+                    <div className="load-more-btn">
+                        <button onClick={this.loadNextPage}>Load Page {this.state.page}</button>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
     }
 }
 
